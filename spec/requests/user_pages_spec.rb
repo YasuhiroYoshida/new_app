9f44a1215ca695ab5e93464c77c78b4aab1363d0
Yasuhiro Yoshida <yasuhiro.yoppu@gmail.com>
2013-06-01 19:37:19 +0200

    Refinish layout and routes with more tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,13 +1,15 @@
 require 'spec_helper'
 
-describe "UserPages" do
-  
-  subject { page }
-  
-  describe "signup page" do
-    before{ visit signup_path }
+include ApplicationHelper
 
-    it {should have_selector('h1', text: 'Sign Up')}
-    it {should have_selector('title', text: full_title('Sign Up'))}
+describe "User pages" do
+
+  subject { page }
+
+  describe "signup page" do
+    before { visit signup_path }
+
+    it { should have_selector('h1', text: 'Sign Up') }
+    it { should have_selector('title', text: full_title('Sign Up')) }
   end
 end
