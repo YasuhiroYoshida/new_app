bba527a5f42e48b883a01b2d6c81a7dba091ebc6
Yasuhiro Yoshida <yasuhiro.yoppu@gmail.com>
2013-06-10 00:16:53 +0200

    Add SSL in production

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -20,11 +20,25 @@ describe "User pages" do
 
     before { visit signup_path }
 
+    let(:user) { FactoryGirl.create(:user) }
     let(:submit) { "Create my account" }
 
     describe "with invalid information" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
+      end
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_selector('title', text: 'Sign Up') }
+        it { should have_content('The form contains') }
+        it { should have_content('error') }
+        it { should have_content('Name can\'t be blank') }
+        it { should have_content('Email can\'t be blank') }
+        it { should have_content('Email is invalid') }
+        it { should have_content('Password can\'t be blank') }
+        it { should have_content('Password is too short (minimum is 6 characters)') }
+        it { should have_content('Password confirmation can\'t be blank') }
       end
     end
 
@@ -38,6 +52,14 @@ describe "User pages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
